b19e396c78e425e04f2b789ea8963251503777a5
Nerzoxxx-dev <nerzoxxxpro@gmail.com>
2021-07-21 13:55:45 +0200

    lib updates

--- lib/src/structures/User.d.ts
+++ lib/src/structures/User.d.ts
@@ -1,11 +1,37 @@
 import { Base } from './Base';
-import { APIUser } from 'discord-api-types';
+import { Snowflake } from '../TUtils/Snowflake';
+import { APIUser, UserFlags, UserPremiumType } from 'discord-api-types';
 import { Client } from '../Client';
 import { Activity } from '../Data/Activity';
 export declare class User extends Base {
-    private _client;
-    private _api;
-    construtor(client: Client, api: APIUser): void;
+    /** User's id */
+    id: Snowflake;
+    /** User's username */
+    username: string;
+    /** User's discriminator */
+    discriminator: string;
+    /** User's avatar */
+    avatar: string | null;
+    /** If the user is a bot */
+    bot?: boolean;
+    /** If the user is an official Discord system */
+    system?: boolean;
+    /** If the user has the 2FA authentification enabled */
+    fa_enabled?: boolean;
+    /** The user language option */
+    language?: string;
+    /** If the user's email is verified */
+    email_verified?: boolean;
+    /** The user's flags. */
+    flags?: UserFlags;
+    /** The user's nitro subscription. It's undefined if he haven't nitro */
+    premium_type: UserPremiumType;
+    /** The user's public Flags */
+    public_flags: UserFlags;
+    constructor(client: Client, api: APIUser);
+    private initData;
+    getAvatarUrl(): string | null;
+    isABot(): boolean;
 }
 export interface UserPresence {
     /** @var number */
